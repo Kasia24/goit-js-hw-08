1f6c963eab7a34d3de8396d30600676ea0709da0
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-08-25 16:23:28 +0200

    add commit

--- gallery.js
+++ gallery.js
@@ -63,32 +63,3 @@ const images = [
     description: "Lighthouse Coast Sea",
   },
 ];
-var modal = document.getElementById("modal");
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var modalImg = document.getElementById("modal-img");
-var captionText = document.getElementById("caption");
-
-var galleryItems = document.getElementsByClassName("gallery-item");
-for (let i = 0; i < galleryItems.length; i++) {
-  galleryItems[i].onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-  };
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
