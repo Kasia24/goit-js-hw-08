488746e46fd4ca73e4ae403ff2ea92ad23b88a13
Kasia24 <160325853+Kasia24@users.noreply.github.com>
2024-09-02 19:56:59 +0200

    add commit

--- task.js
+++ task.js
@@ -3,25 +3,17 @@ const modalImg = document.getElementById("img01");
 const images = document.querySelectorAll(".gallery img");
 const span = document.getElementsByClassName("close")[0];
 
-images.forEach((image) => {
-  image.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-  };
-});
-
 span.onclick = function () {
-  modal.style.display = "none";
-};
-function openModal(imgSrc) {
-  modal.style.display = "block";
-  modalImg.src = imgSrc;
-}
-function closeModal() {
-  modal.style.display = "none";
-  modalImg.src = "";
-}
-closeBtn.onclick = function () {
   closeModal();
 };
-openModal("example.jpg");
+function closeModal() {
+  modal.style.display = "none";
+}
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape") {
+    closeModal();
+  }
+});
+function openModal() {
+  modal.style.display = "block";
+}
